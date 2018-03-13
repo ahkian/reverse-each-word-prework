437a2e326853e172feab05c09cb59f19c2381420
Daniel Brazel <dbrazel4@gmail.com>
2018-03-13 16:57:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   operating_array = string.split(" ")
+  the_returned = []
   operating_array.each do |word|
-    word.reverse
+    the_returned << word.reverse
   end
-  operating_array.join(" ")
+  the_returned.join(" ")
 end
